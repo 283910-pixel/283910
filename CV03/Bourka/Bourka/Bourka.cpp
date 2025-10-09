49cb18025cc521e99d565f1a99ed6a40ba355723
unknown <Student@PC-SE5115-19.utko.feec.vutbr.cz>
2025-10-09 11:20:44 +0200

    CV03

---
+++ CV03/Bourka/Bourka/Bourka.cpp
@@ -0,0 +1,21 @@
+﻿// Bourka.cpp : Defines the entry point for the application.
+//
+
+#include "Bourka.h"
+
+using namespace std;
+
+int main()
+{
+	double cas;
+	double rychlostzvuku = 340;
+	double vzdalenost;
+
+	printf("Zadejte cas v sekundach:");
+	scanf("%lf", &cas);
+	vzdalenost = rychlostzvuku * cas;
+	printf("Vzdalenost hromu je %.2f metru.", vzdalenost);
+
+
+	return 0;
+}
---
+++ CV03/kompexnicisla/kompexnicisla/kompexnicisla.cpp
@@ -0,0 +1,31 @@
+﻿// kompexnicisla.cpp : Defines the entry point for the application.
+//
+
+#include "kompexnicisla.h"
+
+using namespace std;
+
+int main()
+{
+	double ar;
+		double ai;
+		double br;
+		double bi;
+		double souceti;
+		double soucetr;
+
+	printf("Soucet 2 komplexnich cisel\n");
+	printf("Zadej realnou cast prvniho cisla\n");
+			scanf("%lf", &ar);
+			printf("Zadej imaginarni cast prvniho cisla\n");
+			scanf("%lf", &ai);
+			printf("Zadej realnou cast druheho cisla\n");
+			scanf("%lf", &br);
+			printf("Zadej imaginarni cast druheho cisla\n");
+			scanf("%lf", &bi);
+			souceti = ai + bi;
+			soucetr = ar + br;
+			printf("Soucet je %.2f + %.2f i", soucetr, souceti);
+	
+	return 0;
+}
